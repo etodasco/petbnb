746a96372ec1039cf6c9d0fd0da3d01e69ab3ed4
Eric Todasco <erictodasco@me.com>
2024-10-29 19:24:23 -0400

    Added Reservation model & Migrated DB

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root to: "homepage#index"
+    resources :lists, only: [:show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_29_225820) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_29_232152) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "pets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
