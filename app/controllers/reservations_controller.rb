767729ade3b92976a49e4cbc5738862d517d889b
samy boursiquot <samyboursiquot4@gmail.com>
2024-11-02 12:12:11 -0400

    search bar

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,8 @@
+class ReservationsController < ApplicationController
+  def index
+    @reservations = Reservation.all
+  end
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+end
