e12c8d4bff7b401a794ed7e3c85e0953d53418f6
Felisha Jovin <felishanoemie@gmail.com>
2024-11-02 12:12:47 -0400

    petbnb7

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,4 +3,27 @@ class PetsController < ApplicationController
     def index
       @pets = Pet.all
     end
+    def new
+      @pet = Pet.new
+end
+    def create
+      @pet = Pet.new(pet_params)
+      if @pet.save
+        redirect_to @pet, notice: "Pet was successfully created."
+      else
+        render :new
+      end
+    end
+    def show
+      @pet
+    end
+
+    private
+    def set_pet
+      @pet = Pet.find(params[:id])
+    end
+
+    def pet_params
+      params.require(:pet).permit(:name, :breed, :address, :image_url, :description)
+    end
 end
