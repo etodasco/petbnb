f89be417230355bfc5b1dddfa7b6802da605e5ed
Eric Todasco <erictodasco@me.com>
2024-11-04 14:13:23 -0500

    updated pet controller for destroy action

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,41 +1,46 @@
 class PetsController < ApplicationController
-    before_action :authenticate_user!, only: :index
-    def index
-      @pets = Pet.all
-      @markers = @pets.geocoded.map do |flat|
-        {
-          lat: flat.latitude,
-          lng: flat.longitude
-        }
-      end
+  before_action :authenticate_user!, only: :index
+  before_action :set_pet, only: [:show, :destroy]
+  def index
+    @pets = Pet.all
+    @markers = @pets.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
     end
-    def show
-      @pet = Pet.find(params[:id])
-    end
-    def new
-      @pet = Pet.new
-    end
-    def create
-      @pet = Pet.new(pet_params)
-      @pet.user = current_user
-      if @pet.save
-        redirect_to @pet, notice: "Pet was successfully created."
-      else
-        render :new
-      end
-    end
-    private
+  end
 
-    def set_pet
-      @pet = Pet.find(params[:id])
-    end
+  def show
+  end
 
-    def pet_params
-      params.require(:pet).permit(:name, :breed, :address, :image_url, :description)
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to @pet, notice: "Pet was successfully created."
+    else
+      render :new
     end
-    def destroy
-      @pet = Pet.find(params[:id])
-      @pet.destroy
-      redirect_to pets_path, notice: "Pet was successfully removed."
-    end
+  end
+
+  def destroy
+    @pet.destroy
+    redirect_to pets_path, notice: "Pet was successfully removed."
+  end
+
+  private
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+
+  def pet_params
+    params.require(:pet).permit(:name, :breed, :address, :image_url, :description)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   root to: "pets#index"
-  resources :pets, only: [:index, :new, :create, :show] do
+  resources :pets, only: [:index, :new, :create, :show, :destroy] do
     resources :reservations, only: [:new, :create]
   end
   resources :reservations, only: [:index, :show]
