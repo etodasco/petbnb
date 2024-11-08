0132ee17c257f130acdc30d531301bde710b0bf3
Eric Todasco <erictodasco@me.com>
2024-11-08 13:07:39 -0500

    update

--- db/schema.rb
+++ db/schema.rb
@@ -1,18 +1,25 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema[7.2].define(version: 2024_11_06_020229) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_08_174527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "user1_id", null: false
+    t.integer "user2_id", null: false
+    t.integer "pet_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user1_id", "user2_id"], name: "index_conversations_on_user1_id_and_user2_id", unique: true
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content", null: false
+    t.integer "sender_id", null: false
+    t.integer "receiver_id", null: false
+    t.bigint "conversation_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+  end
 
   create_table "pets", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -57,6 +64,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_06_020229) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "conversations"
   add_foreign_key "pets", "users"
   add_foreign_key "reservations", "pets"
   add_foreign_key "reservations", "users"
