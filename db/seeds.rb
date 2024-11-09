114ff94780c7864f96ff7d10e13a317bca12aa01
Eric Todasco <erictodasco@me.com>
2024-11-09 17:07:13 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,6 +34,18 @@ pet_data = [
   { name: "Sadie", breed: "Shih Tzu", address: "707 Boulevard René-Lévesque O, Montreal, QC", description: "Sweet and loves to be pampered.", latitude: 45.4981, longitude: -73.5730, species: "dog", start_date: "2024-12-05", end_date: "2024-12-25" },
 ]
 
+availability_dates = [
+  { available_from: '2024-11-01', available_until: '2024-11-15' },
+  { available_from: '2024-11-05', available_until: '2024-11-20' },
+  { available_from: '2024-11-10', available_until: '2024-11-25' },
+  { available_from: '2024-11-12', available_until: '2024-11-30' },
+  { available_from: '2024-11-15', available_until: '2024-12-01' },
+  { available_from: '2024-11-20', available_until: '2024-12-05' },
+  { available_from: '2024-11-25', available_until: '2024-12-10' },
+  { available_from: '2024-11-28', available_until: '2024-12-15' },
+  { available_from: '2024-12-01', available_until: '2024-12-20' },
+  { available_from: '2024-12-05', available_until: '2024-12-25' },
+]
 
 availability_dates = [
   { available_from: '2024-11-01', available_until: '2024-11-15' },
@@ -48,22 +60,6 @@ availability_dates = [
   { available_from: '2024-12-05', available_until: '2024-12-25' },
 ]
 
-=======
-]
-
-
-availability_dates = [
-  { available_from: '2024-11-01', available_until: '2024-11-15' },
-  { available_from: '2024-11-05', available_until: '2024-11-20' },
-  { available_from: '2024-11-10', available_until: '2024-11-25' },
-  { available_from: '2024-11-12', available_until: '2024-11-30' },
-  { available_from: '2024-11-15', available_until: '2024-12-01' },
-  { available_from: '2024-11-20', available_until: '2024-12-05' },
-  { available_from: '2024-11-25', available_until: '2024-12-10' },
-  { available_from: '2024-11-28', available_until: '2024-12-15' },
-  { available_from: '2024-12-01', available_until: '2024-12-20' },
-  { available_from: '2024-12-05', available_until: '2024-12-25' },
-]
 # Create pet
 pet_data.each_with_index do |data, index|
   pet = Pet.create!(
